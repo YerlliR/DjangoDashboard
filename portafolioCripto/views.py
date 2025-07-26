831ff6b458f5e6c7a428a2b84b68aaebab08afac
Sergio Ricart <sergiricartt@gmail.com>
2025-07-26 20:03:46 +0200

    Mejoras UI paginacion + buscador

--- portafolioCripto/views.py
+++ portafolioCripto/views.py
@@ -2,36 +2,60 @@ from django.shortcuts import render
 from .models import Crypto, Crypto_price
 import requests
 from datetime import datetime
+from django.core.paginator import Paginator 
+from django.db.models import Q  
 
-# Create your views here.
+
 def index(request):
 
-    respones = requests.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    api_page = 1
+    while(api_page <= 4):
+    
+        enlace = f"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page={api_page}&sparkline=false"
+        respones = requests.get(enlace)
 
-    data = respones.json()
+        data = respones.json()
 
-    for crypto_json in data:
+        for crypto_json in data:
 
-        if not Crypto.objects.filter(name=crypto_json['name']).exists():
-            Crypto(
-                name=crypto_json['name'], 
-                symbol=crypto_json['symbol'].upper(), 
-                logo=crypto_json['image'], 
-                deprecated = False
+            if not Crypto.objects.filter(name=crypto_json['name']).exists():
+                Crypto(
+                    name=crypto_json['name'], 
+                    symbol=crypto_json['symbol'].upper(), 
+                    logo=crypto_json['image'], 
+                    deprecated = False
+                ).save()
+
+
+            Crypto_price(
+                price=crypto_json['current_price'], 
+                date=datetime.now(), 
+                crypto= Crypto.objects.get(name=crypto_json['name']),
+                market_cap = crypto_json['market_cap']
             ).save()
+            
+        api_page += 1
+    
+    ui_page = request.GET.get('page', 1)
+    ui_search = request.GET.get('search', '')
 
 
-        Crypto_price(
-            price=crypto_json['current_price'], 
-            date=datetime.now(), 
-            crypto= Crypto.objects.get(name=crypto_json['name']),
-            market_cap = crypto_json['market_cap']
-        ).save()
-        
+    crypto_list = Crypto.objects.all()
 
 
-    crypto_list = list(Crypto.objects.all())
+    if ui_search:
+        crypto_list = crypto_list.filter(
+            Q(name__icontains=ui_search) | 
+            Q(symbol__icontains=ui_search)
+        )
+
+    paginator = Paginator(crypto_list, 50)
+
+    page_obj = paginator.get_page(ui_page)
+
 
     return render(request, 'index.html', {
-        'crypto_list': crypto_list
+        'page_obj': page_obj,
+        'search_query': ui_search, 
+        'total_cryptos': paginator.count, 
         })
